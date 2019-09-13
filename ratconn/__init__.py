7fa1194520191cf1091eefff436764edb5730912
dvm-shlee <shlee@unc.edu>
2019-09-13 11:01:33 -0400

    Setup

--- ratconn/__init__.py
+++ ratconn/__init__.py
@@ -5,7 +5,7 @@ import requests
 from pandas import DataFrame
 from bs4 import BeautifulSoup
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 #%%
 curl_cmd = "curl 'http://neuroviisas.med.uni-rostock.de/connectome/index.php' \
